d3415fca81e75f5632d614385c5b6d3080f8a7e1
Flor96 <31070356+Flor96@users.noreply.github.com>
2017-10-09 01:57:41 -0300

    Add files via upload
    falta corregir

--- lista_ordenada.c
+++ lista_ordenada.c
@@ -4,37 +4,55 @@
 #include "constantes.h"
 #include "lista.h"
 
+int comp;
+
 TListaOrdenada crearlistaordenada(int (*f)(void *,void *)){
 
+
+
     //Asingo el espacio en memoria para la lista
-    TLista lista=(TLista) malloc(sizeof(TListaOrdenada));
+    TListaOrdenada lista_ordenada=(TListaOrdenada) malloc(sizeof(TListaOrdenada));
 
     //Creo la lista.
-    lista->cantidad_elementos=0;
+    lista_ordenada->cantidad_elementos=0;
+    lista_ordenada->lista=crear_lista();
+
+    //Creo variable para comparador.
+    void* num,*num2;
+    int comp=(*f)(num, num2);
+
+    return lista_ordenada;
 
 }
 
 int lo_insertar(TListaOrdenada lista, TElemento elem){
+
+    TCelda nuevaCelda = (TCelda) malloc(sizeof(TCelda));
+    nuevaCelda->elemento=elem;
+    nuevaCelda->proxima_celda=POS_NULA;
 
     if(lista==NULL){
         exit(LST_NO_INI);
     }
 
     if(lista->cantidad_elementos==0){
-        l_insertar(lista, NULL,elem);
+        lo_insertar(lista->lista,elem);
     }
 
     else{
-        //TElemento actual=lista->lista->l_recuperar(elemento);
-        /**TPosicion pos;
-        while(actual!=l_ultima(lista)->elemento){
-            if((*f)(actual,elemento)==-1 || (*f)(actual,elemento)==0){
-                TCelda anterior =l_anterior(lista,pos);
-                TCeldanuevaCelda->proxima_celda=pos;
+        TPosicion pos=lo_primera(lista);
+
+        while(pos!=lo_ultima(lista)){
+            if(comp==1){
+                lista->lista->primera_celda=nuevaCelda;
+            }
+            else{
+                pos=lo_siguiente(lista,pos);
+                nuevaCelda->proxima_celda=pos;
                 anterior->proxima_celda=nuevaCelda;
             }
 
-        }**/
+        }
     }
 
 
@@ -58,10 +76,6 @@ TPosicion lo_ultima(TListaOrdenada lista){
 TPosicion lo_siguiente(TListaOrdenada lista,TPosicion pos){
 
     return l_siguiente(lista->lista,pos);
-}
-
-TPosicion lo_anterior(TListaOrdenada lista, TPosicion pos){
-    return l_anterior(lista->lista,pos);
 }
 
 int lo_size(TListaOrdenada lista){
