5aa3c8a8077c0768455b73ec58a487604823e8b7
Flor96 <31070356+Flor96@users.noreply.github.com>
2017-10-08 21:30:51 -0300

    Add files via upload
    falta ver como usar el comparador en una variable y como asignar un nuevo elemento en una determinada posicion de la lista

--- lista_ordenada.c
+++ lista_ordenada.c
@@ -7,19 +7,36 @@
 TListaOrdenada crearlistaordenada(int (*f)(void *,void *)){
 
     //Asingo el espacio en memoria para la lista
-    TListaOrdenada lista=(TListaOrdenada) malloc(sizeof(TListaOrdenada));
+    TLista lista=(TLista) malloc(sizeof(TListaOrdenada));
 
     //Creo la lista.
     lista->cantidad_elementos=0;
-	lista->primera_celda=POS_NULA;
-
-	//Asingo la funcion de comparaciopn a una variable.
-	int comp=(*f)
 
 }
 
 int lo_insertar(TListaOrdenada lista, TElemento elem){
 
+    if(lista==NULL){
+        exit(LST_NO_INI);
+    }
+
+    if(lista->cantidad_elementos==0){
+        l_insertar(lista, NULL,elem);
+    }
+
+    else{
+        TElemento actual=lista->lista->l_recuperar(elemento);
+        TPosicion pos;
+        while(actual!=l_ultima(lista)->elemento){
+            if((*f)(actual,elemento)==-1 || (*f)(actual,elemento)==0){
+                TCelda anterior =lista->lista->l_anterior(lista,pos);
+                TCeldanuevaCelda->proxima_celda=pos;
+                anterior->proxima_celda=nuevaCelda;
+            }
+
+        }
+    }
 
 
+    return TRUE;
 }
