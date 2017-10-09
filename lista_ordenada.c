f1157fcac23ae4753b617e7252e40d1b090dc83b
Flor96 <31070356+Flor96@users.noreply.github.com>
2017-10-08 23:49:01 -0300

    Add files via upload
    lista ordenada implementada solo falta terminar insertar (te amo)

--- lista_ordenada.c
+++ lista_ordenada.c
@@ -25,18 +25,45 @@ int lo_insertar(TListaOrdenada lista, TElemento elem){
     }
 
     else{
-        TElemento actual=lista->lista->l_recuperar(elemento);
-        TPosicion pos;
+        //TElemento actual=lista->lista->l_recuperar(elemento);
+        /**TPosicion pos;
         while(actual!=l_ultima(lista)->elemento){
             if((*f)(actual,elemento)==-1 || (*f)(actual,elemento)==0){
-                TCelda anterior =lista->lista->l_anterior(lista,pos);
+                TCelda anterior =l_anterior(lista,pos);
                 TCeldanuevaCelda->proxima_celda=pos;
                 anterior->proxima_celda=nuevaCelda;
             }
 
-        }
+        }**/
     }
 
 
     return TRUE;
+}
+
+int lo_eliminar(TListaOrdenada lista, TPosicion pos){
+
+    return l_eliminar(lista,pos);
+}
+
+TPosicion lo_primera(TListaOrdenada lista){
+
+    return l_primera(lista);
+}
+
+TPosicion lo_ultima(TListaOrdenada lista){
+    return l_ultima(lista);
+}
+
+TPosicion lo_siguiente(TListaOrdenada lista,TPosicion pos){
+
+    return l_siguiente(lista,pos);
+}
+
+TPosicion lo_anterior(TListaOrdenada lista, TPosicion pos){
+    return l_anterior(lista,pos);
+}
+
+int lo_size(TListaOrdenada lista){
+    return l_size(lista);
 }
