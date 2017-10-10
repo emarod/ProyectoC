ede36c89ec76c7ba7414ef6f19eb521bd6db3392
Alumno <alumno@ocuns>
2017-10-10 16:17:31 -0300

    update interfaz

--- evaluador.c
+++ evaluador.c
@@ -35,11 +35,11 @@ int main()
         //printf("%s ",car);
     }
 
+    fclose(arch);
+
 
 
     printf("\n\nEl archivo se ha leido correctamente.\n\n");
-
-    rewind(arch);
 
     char *cadena1;
     char *cadena2;
@@ -48,7 +48,9 @@ int main()
     char *cadena5;
     char *cadena6;
 
-
+    arch=fopen(nombre,"rb");
+    int opcion=0;
+    while (opcion!=6){
     printf("----------------MENU DE PROGRAMA----------------\n");
 
     printf("(1) Mostrar Palabras.\n");
@@ -59,43 +61,57 @@ int main()
     printf("(6) Salir.\n");
 
     printf("Elija una opcion del (1) al (6):\n");
-    int opcion;
+
     scanf("%i",&opcion);
 
     switch(opcion){
     case 1:{
-
+            printf("%i",opcion);
             while(feof(arch)==0){
             fscanf(arch,"%s",cadena1);
-            printf("%s                         %i\n",car,tr_recuperar(trie,cadena1));
+            printf("%s                         %i\n",cadena1,tr_recuperar(trie,cadena1));
             }
 
             break;}
     case 2:{
-            char *palabra;
             printf("Ingrese palabra para consultar si pertenece: \n");
-            scanf("%s", palabra);
-            if(tr_pertenece(trie,palabra)==1) {
-                printf("La palabra %s pertenece al archivo y aparece %i veces.\n\n",palabra,tr_recuperar(trie,palabra));
+            scanf("%s", &cadena2);
+            printf("%s",cadena2);
+            if(tr_pertenece(trie,cadena2)==1) {
+                printf("La palabra %s pertenece al archivo y aparece %i veces.\n\n",cadena2,tr_recuperar(trie,cadena2));
             }
             else{
-                printf("La palabra %s no pertenece al archivo.\n\n",palabra);
+                printf("La palabra %s no pertenece al archivo.\n\n",cadena2);
             }
             break;}
 
     case 3:{
+            char c=NULL;
+            int cant=0;
+
+            printf("Ingrese letra:\n");
+            scanf("%c",&c);
+
             rewind(arch);
-            char *cadena2;
             while(feof(arch)==0){
-            fscanf(arch,"%s",cadena2);
+            fscanf(arch,"%s",cadena3);
+            printf("%s \n",cadena3);
+            //if(cadena3̣[0]==c) {cant++;}
+            }
+            printf("La cantidad de veces que %c aparece en el archivo es %i.",c,cant);
+            }
+
+    case 4:{
+
             }
 
 
-
-            }
 
 
     }
+}
+
+printf("Adiós.\n");
 
 
 
